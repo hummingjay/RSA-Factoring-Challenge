1e3413a4d69c1c33a0d14ab695dd900b1f652083
hummingjay <jays7mill@gmail.com>
2023-03-31 09:56:03 +0300

    check prime and with good indent

--- Rlab.py
+++ Rlab.py
@@ -13,12 +13,30 @@ def fct(num):
     # add step size of 2 to remove even numbers in loop
     for i in range(3, int(num**0.5) + 1, 2):
         if num % i == 0:
-            return (i, num//i)
+            if prime(i) and prime(num//i):
+                return (i, num//i)
     return None
-
+def prime(lim):
+    """
+    Returns true if num is a prime number, false if not
+    """
+    if lim < 2:
+        return False
+    elif lim == 2:
+        return True
+    elif lim % 2 == 0:
+        return False
+    else:
+        for i in range(3, int(lim**0.5) + 1, 2):
+            if lim % i == 0:
+                return False
+        return True
 file_name = sys.argv[1]
 
 with open(file_name, 'r') as file:
     for line in file:
         number = fct(int(line.strip()))
-        print("{}={}*{}".format(line.strip(), number[1], number[0]))
+        if number is None:
+            print("{} has no prime factors.".format(line.strip()))
+        else:
+            print("{}={}*{}".format(line.strip(), number[1], number[0]))
